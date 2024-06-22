506f63d3223f1dd2f9aaa819fe537948f3775b4e
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-22 15:22:41 -0600

    import Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import './App.css'
 import GlobalStyles from "./GlobalStyles";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <GlobalStyles id="global-styles" >
+        <Header />
     </GlobalStyles>
   );
 };
