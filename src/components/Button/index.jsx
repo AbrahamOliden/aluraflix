e5ec2b6e9d9a8a3ec769c11995d5090287500571
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-22 16:15:09 -0600

    first draft for Button styled component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    min-width: 150px;
+    padding: 15px 0;
+    font-size: 1rem;
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+    border: 2px solid var(--white);
+    border-radius: 10px;
+    background-color: inherit;
+    color: inherit;
+    cursor: pointer;
+`;
+
+export default StyledButton;
