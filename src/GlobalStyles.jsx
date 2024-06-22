fbb9f30996e4ec12d0b0d9d4005f8c79c18fa536
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-22 16:03:16 -0600

    add dark-gray and white variables

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 const GlobalStyles = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
     background-color: var(--dark-gray);
+    color: var(--white);
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0;
+    text-align: center;
 `;
 
 export default GlobalStyles;
