59bd832944e58532914c7ea41f0aba9da40262ed
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-23 20:35:45 -0600

    basic styling done

--- src/components/CarouselComponents/VideoCard/index.jsx
+++ src/components/CarouselComponents/VideoCard/index.jsx
@@ -3,24 +3,34 @@ import styled from "styled-components";
 const StyledElement = styled.li`
     width: 30%;
     height: 300px;
+    figure {
+        margin: 0;
+    }
     img {
         width: 100%;
-        border: 1px solid ${props => props.$color};
+        border: 3px solid ${props => props.$color};
         border-radius: 15px 15px 0 0;
+        box-sizing: border-box;
     }
     figcaption {
         display: flex;
         flex-flow: row nowrap;
+        justify-content: space-evenly;
+        align-items: center;
+        padding: 16px;
         background-color: var(--black);
-        border: 1px solid ${props => props.$color};
+        border: 3px solid ${props => props.$color};
         border-radius: 0 0 15px 15px;
         button {
             font-size: 1rem;
             font-weight: 400;
             background-color: transparent;
+            color: inherit;
+            border: none;
             img {
                 width: 1rem;
                 margin-right: 1rem;
+                border: none;
             }
         }
     }
