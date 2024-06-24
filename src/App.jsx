94283c263e285b1fe4ef783c3dfb7e7a01248d7b
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-23 20:00:39 -0600

    add various Carousel components to simulate mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 import GlobalStyles from "./GlobalStyles";
 import Banner from "./components/Banner";
+import Carousel from "./components/CarouselComponents/Carousel";
 import DefaultPage from "./components/DefaultPage";
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
     <GlobalStyles id="global-styles" >
         <DefaultPage>
           <Banner />
+          <Carousel text={"Front End"} color={"#6BD1FF"} />
+          <Carousel text={"Back End"} color={"#00C86F"} />
+          <Carousel text={"Innovacion & Gestion"} color={"#FFBA05"} />
         </DefaultPage>
     </GlobalStyles>
   );
