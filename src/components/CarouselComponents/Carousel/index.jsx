5fcf521f41003405dc780d628157cea8dc67ff2f
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-23 19:17:21 -0600

    first draft of Carousel component

---
+++ src/components/CarouselComponents/Carousel/index.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+const StyledCarousel = styled.section`
+     display: flex;
+     flex-flow: column nowrap;
+     justify-content: flex-start;
+     button {
+        min-width: fit-content;
+        width: 350px;
+        background-color: ${props => props.$color};
+        color: var(--white);
+        font-size: 1.5rem;
+        font-weight: 700;
+        text-align: center;
+        text-transform: uppercase;
+        padding: 20px;
+        border: none;
+        border-radius: 15px;
+        cursor: pointer;
+     }
+     ul {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 30px;
+        list-style: none;
+     }
+`;
+
+function Carousel({ text, color }) {
+    return (
+        <StyledCarousel $color={color} >
+            <button >{text}</button>
+            <ul>
+                <li>Video 01</li>
+                <li> Video 02</li>
+                <li>Video 03</li>
+            </ul>
+        </StyledCarousel>
+    )
+}
+
+export default Carousel
