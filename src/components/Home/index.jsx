c0d6ff10394c80afe1cc7dd6241b9cff62a3da0a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-24 20:23:18 -0600

    remove Banner

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,8 @@
-import Banner from "../Banner";
 import Carousel from "../CarouselComponents/Carousel";
 
 function Home() {
     return (
         <main>
-            <Banner />
             <Carousel text={"Front End"} color={"#6BD1FF"} />
             <Carousel text={"Back End"} color={"#00C86F"} />
             <Carousel text={"Innovacion & Gestion"} color={"#FFBA05"} />
