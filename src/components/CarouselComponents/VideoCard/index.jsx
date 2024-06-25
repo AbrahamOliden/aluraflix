780a9f5ff14dc78b245f076399335179f0722c0a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-24 20:09:08 -0600

    create StyledFigure, StyledImage, and StyledCaption for cleaner code

--- src/components/CarouselComponents/VideoCard/index.jsx
+++ src/components/CarouselComponents/VideoCard/index.jsx
@@ -1,50 +1,50 @@
 import styled from "styled-components";
 
-const StyledElement = styled.li`
-    figure {
-        margin: 0;
-        display: flex;
-        flex-flow: column nowrap;
-        gap: 0;
-        width: 25vw;
-        margin-bottom: 20px;
-    }
-    img {
-        width: 100%;
-        border: 3px solid ${props => props.$color};
-        border-radius: 15px 15px 0 0;
-        box-sizing: border-box;
-    }
-    figcaption {
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: space-evenly;
-        align-items: center;
-        padding: 16px;
-        background-color: var(--black);
-        border: 3px solid ${props => props.$color};
-        border-radius: 0 0 15px 15px;
-        button {
-            font-size: 1rem;
-            font-weight: 400;
-            background-color: transparent;
-            color: inherit;
+const StyledFigure = styled.figure`
+    margin: 0;
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 0;
+    width: 25vw;
+    margin-bottom: 20px;
+`;
+
+const StyledImage = styled.img`
+    width: 100%;
+    border: 3px solid ${props => props.$color};
+    border-radius: 15px 15px 0 0;
+    box-sizing: border-box;
+`;
+
+const StyledCaption = styled.figcaption`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 16px;
+    background-color: var(--black);
+    border: 3px solid ${props => props.$color};
+    border-radius: 0 0 15px 15px;
+    button {
+        font-size: 1rem;
+        font-weight: 400;
+        background-color: transparent;
+        color: inherit;
+        border: none;
+        img {
+            width: 1rem;
+            margin-right: 1rem;
             border: none;
-            img {
-                width: 1rem;
-                margin-right: 1rem;
-                border: none;
-            }
         }
     }
 `;
 
 function VideoCard({ color }) {
   return (
-    <StyledElement $color={color} >
-        <figure>
-            <img src="/miniature01.jpg" alt="Video title" />
-            <figcaption>
+    <li>
+        <StyledFigure>
+            <StyledImage src="/miniature01.jpg" alt="Video title" $color={color} />
+            <StyledCaption $color={color}>
                 <button>
                     <img src="/trash.png" alt="trash icon" />
                     Delete
@@ -53,9 +53,9 @@ function VideoCard({ color }) {
                     <img src="/edit.png" alt="edit icon" />
                     Edit
                 </button>
-            </figcaption>
-        </figure>
-    </StyledElement>
+            </StyledCaption>
+        </StyledFigure>
+    </li>
   )
 }
 
