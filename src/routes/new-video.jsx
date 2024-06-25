ad4f43a446cffbddc681c3c59e68e701122cd638
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 17:59:35 -0600

    create route for new video

---
+++ src/routes/new-video.jsx
@@ -0,0 +1,13 @@
+import DefaultPage from "../components/DefaultPage";
+import NewVideo from "../components/NewVideo";
+
+
+function NewVideoRoute() {
+  return (
+    <DefaultPage>
+      <NewVideo />
+    </DefaultPage>
+  );
+};
+
+export default NewVideoRoute;
