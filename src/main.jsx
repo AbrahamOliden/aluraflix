25efd0617dc4bb89f08674d44f1931bd8f75a09a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-24 21:32:42 -0600

    use React Router to access app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root.jsx';
+import NotFound from './routes/not-found.jsx';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "new-video",
+        element: <h1>ola</h1>,
+        errorEelement: <NotFound />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
