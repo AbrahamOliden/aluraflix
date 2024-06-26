21ca5490482af5f8c987ef735e81063bc0073f28
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-26 16:43:12 -0600

    create route for new category

---
+++ src/routes/new-category.jsx
@@ -0,0 +1,14 @@
+import DefaultPage from "../components/DefaultPage";
+import NewCategory from "../components/Registry/NewCategory";
+
+import React from 'react'
+
+function NewCategoryRoute() {
+  return (
+    <DefaultPage>
+        <NewCategory />
+    </DefaultPage>
+  )
+}
+
+export default NewCategoryRoute
