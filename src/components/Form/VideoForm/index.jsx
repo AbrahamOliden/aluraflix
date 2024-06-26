fa16b1b6789c07dff6e702d267579e24b853a8a7
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 22:17:24 -0600

    remove hardcoded inputs and use Input component instead

--- src/components/Form/VideoForm/index.jsx
+++ src/components/Form/VideoForm/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import StyledButton from "../../Button";
+import Input from "../../InputField";
 
 const StyledField = styled.fieldset`
     display: flex;
@@ -25,29 +26,38 @@ function VideoForm() {
     return (
         <form>
             <StyledField>
-                <FlexContainer role="none">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" name="title" placeholder="Title or smth" />
-                </FlexContainer>
-
-                <FlexContainer role="none">
-                    <label htmlFor="category">Category</label>
-                    <input type="text" name="category" placeholder="category" />
-                </FlexContainer>
-
-                <FlexContainer role="none">
-                    <label htmlFor="image">Image</label>
-                    <input type="text" name="image" placeholder="image " />
-                </FlexContainer>
-
-                <FlexContainer role="none">
-                    <label htmlFor="source">source</label>
-                    <input type="text" name="source" placeholder="source" />
-                </FlexContainer>
-                <FlexContainer $width="100%" role="none" >
-                    <label htmlFor="description">description</label>
-                    <textarea name="description" id="description" cols="50" rows="10"></textarea>
-                </FlexContainer>
+                <Input inputsArray = {[
+                    {
+                        title: "title",
+                        type: "text",
+                        required: true,
+                        id: 0
+                    },
+                    {
+                        title: "category",
+                        type: "select",
+                        required: true,
+                        id: 1
+                    },
+                    {
+                        title: "image",
+                        type: "text",
+                        required: true,
+                        id: 2
+                    },
+                    {
+                        title: "video",
+                        type: "text",
+                        required: true,
+                        id: 3
+                    },
+                    {
+                        title: "description",
+                        type: "textarea",
+                        required: false,
+                        id: 4
+                    }
+                ]} />
 
                 <StyledButton type="submit" >Submit</StyledButton>
                 <StyledButton>Clear</StyledButton>
