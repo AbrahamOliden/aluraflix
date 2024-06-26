83acc77c63e54e4a8c6cd79adce3c0caa6defbc4
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 19:42:25 -0600

    create StyledSection component

--- src/components/NewVideo/index.jsx
+++ src/components/NewVideo/index.jsx
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 import VideoForm from "../Form/VideoForm";
 
+const StyledSection = styled.section`
+  min-height: fit-content;
+  padding: 50px 100px;
+  &>h1 {
+    text-transform: uppercase;
+  }
+`;
+
 function NewVideo() {
   return (
-    <section>
-        <h1>New Video</h1>
-        <p>Fill the form to create a new video card</p>
-        <VideoForm />
-    </section>
+    <StyledSection>
+      <h1>New Video</h1>
+      <p>Fill the form to create a new video card</p>
+      <VideoForm />
+    </StyledSection>
   );
 };
 
