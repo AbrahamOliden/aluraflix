2e27d729fb937a375267502e3ab4a4caa664ec0c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 22:16:28 -0600

    map inputsArray and use switch to return inputs

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -11,31 +11,106 @@ const FlexContainer = styled.div`
     }
 `;
 
-const StyledLabel = styled.label`
+const StyledLabel = styled.label` 
+    margin: 0 0 16px 5px;
     color: var(--white);
     font-size: 1.2rem;
-    font-weight: 500;
+    font-weight: 600;
     line-height: 24px;
+    text-transform: capitalize;
 `;
 
-const StyledInput = styled.input`
+const StyledInput = styled.input` //? All these styles might be combined
     background-color: var(--dark-gray);
+    color: var(--white);
+    font-size: 1.2rem;
+    font-weight: 400;
     padding: 16px;
+    border: 2px solid var(--blue);
+    border-radius: 10px;
 `;
 
-function Input({inputsArray}) {
+const StyledDropdown = styled.select` //? and then extend in styled components
+    background-color: var(--dark-gray);
+    color: var(--white);
+    font-size: 1.2rem;
+    font-weight: 400;
+    padding: 16px;
+    border: 2px solid var(--blue);
+    border-radius: 10px;
+`;
+
+const StyledTextArea = styled.textarea` //? this 1 2
+    width: 50%;
+    min-width: 350px;
+    background-color: var(--dark-gray);
+    color: var(--white);
+    font-size: 1.2rem;
+    font-weight: 400;
+    padding: 16px;
+    border: 2px solid var(--blue);
+    border-radius: 10px;
+`;
+
+function Input({ inputsArray }) {
 
     return (
-        {
-            ...inputsArray.map(input => {
-                return (
-                    <FlexContainer key={input.id} role="none" >
-                        <StyledLabel htmlFor={input.title}> {input.title} </StyledLabel>
-                        <StyledInput type={input.type} name={input.title} ></StyledInput>
-                    </FlexContainer>
-                )
-            })
-        }
+        <>
+            {inputsArray.map(input => {
+
+                switch (input.type) {
+                    case "text":
+                        return ( //? This better be combined
+                            <FlexContainer key={input.id} role="none" >
+                                <StyledLabel htmlFor={input.title}>
+                                    {input.title}
+                                </StyledLabel>
+
+                                <StyledInput
+                                    type={input.type}
+                                    name={input.title}
+                                    placeholder={`Introduce ${input.title}`}
+                                    required={input.required} >
+                                </StyledInput>
+                            </FlexContainer>
+                        );
+                    case "select":
+                        return (
+                            <FlexContainer key={input.id} role="none" >
+                                <StyledLabel htmlFor={input.title}>
+                                    {input.title}
+                                </StyledLabel>
+
+                                <StyledDropdown
+                                    type={input.type}
+                                    name={input.title}
+                                    placeholder={`Introduce ${input.title}`} 
+                                    required={input.required} >
+                                </StyledDropdown>
+                            </FlexContainer>
+                        );
+                    case "textarea":
+                        return (
+                            <FlexContainer key={input.id} role="none" $width="100%" >
+                                <StyledLabel htmlFor={input.title}>
+                                    {input.title}
+                                </StyledLabel>
+
+                                <StyledTextArea
+                                    type={input.type}
+                                    name={input.title}
+                                    placeholder={`Introduce ${input.title}`} 
+                                    cols={50}
+                                    rows={10}
+                                    required={input.required}>
+                                </StyledTextArea>
+                            </FlexContainer>
+                        )
+                    default:
+                        return;
+                }
+            })}
+        </>
     );
 };
 
