d2e8af8e1c0f389c9a2412e40634eb84e3d70490
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 18:03:21 -0600

    remove GlobalStyles and DeafultPage (subject to changes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-// import './App.css'
-import GlobalStyles from "./GlobalStyles";
-import DefaultPage from "./components/DefaultPage";
 import Banner from "./components/Banner";
 import Home from "./components/Home";
 
 function App() {
 
   return (
-    <GlobalStyles id="global-styles" >
-        <DefaultPage id="default-page" >
+        <>
           <Banner id="banner" />
           <Home />
-        </DefaultPage>
-    </GlobalStyles>
+        </>
   );
 };
 
