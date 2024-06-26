cd39fbe620783e6b7ff02a1b9e3282c02cb3586e
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-25 18:00:36 -0600

    import GlobalStyles (subject to changes)

--- src/components/DefaultPage/index.jsx
+++ src/components/DefaultPage/index.jsx
@@ -1,13 +1,14 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import GlobalStyles from "../../GlobalStyles";
 
 function DefaultPage({ children }) {
   return (
-    <>
+    <GlobalStyles id="global-styles" >
         <Header />
         {children}
         <Footer />
-    </>
+    </GlobalStyles>
   );
 };
 
