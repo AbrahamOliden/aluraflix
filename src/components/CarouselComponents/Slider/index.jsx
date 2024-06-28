9ab732580d05849355238f7bc0da2ff458ae86d4
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-27 20:20:44 -0600

    map data's videos array to render VideoCardSlider

--- src/components/CarouselComponents/Slider/index.jsx
+++ src/components/CarouselComponents/Slider/index.jsx
@@ -1,19 +1,27 @@
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import VideoCardSlider from "../VideoCard";
+import data from "../slider-data.json";
 
 function MySlider() {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 3000,
+        speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1
     };
 
     return (
-        <Slider {...settings} >
+        <div className="slider-container" style={{ maxWidth: "100%" }} >
+            <Slider {...settings} >
+                {
+                    data.videos.map(video => <VideoCardSlider key={video.id} {...video} />)
+                }
+            </Slider>
+        </div>
+    );
+};
 
-        </Slider>
-    )
-}
-
-export default MySlider
+export default MySlider;
