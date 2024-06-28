2d05dbc740d604bbbfc782803ad6d1cd70b5d781
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-27 18:54:13 -0600

    create VideoCardSlider component and some styled components

---
+++ src/components/CarouselComponents/VideoCard/index.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const StyledFigure = styled.figure`
+    display: flex;
+    flex-flow: row-reverse nowrap;
+    &>img {
+        flex: 1;
+    };
+`;
+
+const StyledCaption = styled.figcaption`
+    flex: 1;
+`;
+
+const StyledButton = styled.button`
+    min-width: 420px;
+    background-color: ${props => props.$color};
+    color: var(--white);
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+    padding: 20px;
+    border: none;
+    border-radius: 15px;
+    cursor: pointer;
+`;
+
+function VideoCardSlider(props) {
+    const { title, path, image, description, category } = props;
+
+    const buttonColor = {
+        "Front-end": "#6BD1FF",
+        "Back-end": "#00C86F",
+        "Innovation-management": "#FFBA05"
+    };
+
+    return (
+        <StyledFigure>
+            <img src={image} alt={title} />
+            <StyledCaption>
+                <StyledButton $color={buttonColor[category]} >
+                    {category}
+                </StyledButton>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </StyledCaption>
+        </StyledFigure>
+    )
+}
+
+export default VideoCardSlider
