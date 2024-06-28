65781f6d89b59fbdec2be005aa2b78f274dd22ea
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-27 20:22:33 -0600

    import MySlider (definitely will change)

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -35,12 +35,6 @@ function Banner() {
         <img src="/banner.png" alt="background image" />
         <StyledSection>
             <MySlider />
-            {/* <div>
-                <button>Front End</button>
-                <h1>Challenge React</h1>
-                <p>Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</p>
-            </div>
-            <img src="/test-image-video.png" alt="bidio" /> */}
 
         </StyledSection>
         <section>
