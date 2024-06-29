68aaf8cf5fc2b577e635484b5d2955599d72e841
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-28 23:16:01 -0600

    map on each video to return a VideoCard

--- src/components/CarouselComponents/Slider/index.jsx
+++ src/components/CarouselComponents/Slider/index.jsx
@@ -2,22 +2,61 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import VideoCardSlider from "../VideoCard";
+import styled from "styled-components";
 import data from "../slider-data.json";
 
-function MySlider() {
+const StyledList = styled.ul`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+    gap: 40px;
+    list-style: none;
+`;
+
+function MySlider(props) {
     const settings = {
         dots: true,
         infinite: true,
         speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
+
+    const data = Object.values(props).filter(item => typeof (item) === "object");
 
     return (
         <div className="slider-container" style={{ maxWidth: "100%" }} >
             <Slider {...settings} >
                 {
-                    data.videos.map(video => <VideoCardSlider key={video.id} {...video} />)
+                    data.map(video => {
+                        return <VideoCardSlider key={video.id} color={props.color} {...video} />
+                    })
                 }
             </Slider>
         </div>
