5d52b7c0d7cb19a17dff5befd04a70c687cacbe0
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-28 23:17:40 -0600

    create first stable version of VideoCard

--- src/components/CarouselComponents/VideoCard/index.jsx
+++ src/components/CarouselComponents/VideoCard/index.jsx
@@ -1,48 +1,90 @@
 import styled from "styled-components";
 
+// const StyledFigure = styled.figure`
+//     display: flex;
+//     flex-flow: column nowrap;
+// `;
+
+// const StyledCaption = styled.figcaption`
+//     flex: 1;
+// `;
+
+// const StyledButton = styled.button`
+//     min-width: 420px;
+//     background-color: ${props => props.$color};
+//     color: var(--white);
+//     font-size: 2rem;
+//     font-weight: 700;
+//     text-align: center;
+//     text-transform: uppercase;
+//     padding: 20px;
+//     border: none;
+//     border-radius: 15px;
+//     cursor: pointer;
+// `;
+
 const StyledFigure = styled.figure`
+    width: 25vw;
+    margin: 0;
     display: flex;
-    flex-flow: row-reverse nowrap;
-    &>img {
-        flex: 1;
-    };
+    flex-flow: column nowrap;
+    gap: 0;
+    margin-bottom: 20px;
+    &>div {
+        box-shadow: inset 0 0 20px 5px ${props => props.$color};
+        border-radius: 15px 15px 0 0;
+    }
+`;
+
+const StyledImage = styled.img`
+    position: relative;
+    width: 100%;
+    border: 3px solid ${props => props.$color};
+    border-radius: 15px 15px 0 0;
+    box-sizing: border-box;
+    z-index: -1;
 `;
 
 const StyledCaption = styled.figcaption`
-    flex: 1;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 16px;
+    background-color: var(--black);
+    border: 3px solid ${props => props.$color};
+    border-radius: 0 0 15px 15px;
+    box-sizing: border-box;
+    button {
+        font-size: 1rem;
+        font-weight: 400;
+        background-color: transparent;
+        color: inherit;
+        border: none;
+        img {
+            width: 1rem;
+            margin-right: 1rem;
+            border: none;
+        }
+    }
 `;
 
-const StyledButton = styled.button`
-    min-width: 420px;
-    background-color: ${props => props.$color};
-    color: var(--white);
-    font-size: 2rem;
-    font-weight: 700;
-    text-align: center;
-    text-transform: uppercase;
-    padding: 20px;
-    border: none;
-    border-radius: 15px;
-    cursor: pointer;
-`;
-
-function VideoCardSlider({ title, image, description, category }) {
-
-    const buttonColor = {
-        "Front-end": "#6BD1FF",
-        "Back-end": "#00C86F",
-        "Innovation-management": "#FFBA05"
-    };
+function VideoCardSlider(props) {
 
     return (
-        <StyledFigure>
-            <img src={image} alt={title} />
-            <StyledCaption>
-                <StyledButton $color={buttonColor[category]} >
-                    {category}
-                </StyledButton>
-                <h2>{title}</h2>
-                <p>{description}</p>
+        <StyledFigure $color={props.color}>
+            <div role="none" >
+                <StyledImage src={props.image} alt={props.title} />
+            </div>
+            <StyledCaption $color={props.color} >
+                <button>
+                    <img src="/edit.png" alt="Edit icon" />
+                    Edit
+                </button>
+                <button>
+                    <img src="/trash.png" alt="Delete icon" />
+                    Delete
+                </button>
             </StyledCaption>
         </StyledFigure>
     )
