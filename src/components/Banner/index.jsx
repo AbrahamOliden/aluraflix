6269e5ad62658a93e3a10e0fca718930cdc67a50
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-28 19:18:03 -0600

    Banner returned to original design

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -28,19 +28,64 @@ const StyledSection = styled.section`
     z-index: 1;
 `;
 
+const StyledFigure = styled.figure`
+    display: flex;
+    flex-flow: row-reverse nowrap;
+    justify-content: space-evenly;
+    width: 100%;
+    &>img {
+        flex: 1;
+        flex-shrink: 2;
+    };
+`;
+
+const StyledCaption = styled.figcaption`
+    flex: 1;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-evenly;
+    &>#category-banner {
+        font-size: 2.5rem;
+        margin: 0;
+    };
+    &>#category-description {
+        font-size: 1.2rem;
+        margin: 0;
+    }
+`;
+
+const StyledHeading = styled.h2`
+    width: fit-content;
+    margin: 0 auto 0 0;
+    background-color: #6BD1FF;
+    color: var(--white);
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+    padding: 20px 60px;
+    border: none;
+    border-radius: 15px;
+`;
+
 
 function Banner() {
-  return (
-    <StyledContainer >
-        <img src="/banner.png" alt="background image" />
-        <StyledSection>
-            <MySlider />
+    return (
+        <StyledContainer >
+            <img src="/banner.png" alt="background image" />
+            <StyledSection>
+                <StyledFigure id="styled-figure" >
+                    <img src="/test-image-video.png" alt="video miniature" />
+                    <StyledCaption>
+                        <StyledHeading >Front end</StyledHeading>
+                        <h3 id="category-banner" >Challenge React</h3>
+                        <p id="category-description" >Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</p>
+                    </StyledCaption>
+                </StyledFigure>
 
-        </StyledSection>
-        <section>
-        </section>
-    </StyledContainer>
-  )
-}
+            </StyledSection>
+        </StyledContainer>
+    );
+};
 
-export default Banner
+export default Banner;
