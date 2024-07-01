c0897d28d87c91f90999cdf677e8941dbbc861c1
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-01 17:36:42 -0600

    create GlobalContextProvider as deafult export

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,3 +1,13 @@
 import { createContext } from "react";
 
 const GlobalContext = createContext();
+
+const GlobalContextProvider = ({ children }) => {
+    return (
+        <GlobalContext.Provider>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
+
+export default GlobalContextProvider;
