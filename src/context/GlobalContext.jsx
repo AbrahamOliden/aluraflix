b75198552594e43cb594c52a77b4ccd66cc2c6a5
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-01 17:52:06 -0600

    create async function getData

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,10 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
+
+    const getData = async () => {
+        const resolve = await fetch("http://localhost:5000/categories");
+        const data = await resolve.json();
+        console.log(data);
+    };
+
     return (
-        <GlobalContext.Provider>
+        <GlobalContext.Provider value={getData} >
             {children}
         </GlobalContext.Provider>
     );
