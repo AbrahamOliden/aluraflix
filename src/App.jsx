33327641e788cae253242a24dfc3e29a8ad75375
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-01 19:18:23 -0600

    import GlobalContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import GlobalContextProvider from "./context/GlobalContext";
 import Banner from "./components/Banner";
 import Home from "./components/Home";
 
 function App() {
 
   return (
-        <>
+        <GlobalContextProvider>
           <Banner id="banner" />
           <Home />
-        </>
+        </GlobalContextProvider>
   );
 };
 
