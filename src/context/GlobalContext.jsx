0cf79809db0a7fcb82007d7d5c88de0158c2837f
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-01 18:33:54 -0600

    move getData inside useEffect

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -4,14 +4,24 @@ const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
 
-    const getData = async () => {
-        const resolve = await fetch("http://localhost:5000/categories");
-        const data = await resolve.json();
-        console.log(data);
-    };
+    const [videos, setVideos] = useState([]);
+
+    
+    useEffect(() => {
+        const getData = async () => {
+            const resolve = await fetch("http://localhost:5000/categories");
+            const data = await resolve.json();
+            setVideos([...data]);
+        };
+
+        setTimeout(() => {
+            getData();
+            console.log(videos);
+        });
+    }, []);
 
     return (
-        <GlobalContext.Provider value={getData} >
+        <GlobalContext.Provider value={videos} >
             {children}
         </GlobalContext.Provider>
     );
