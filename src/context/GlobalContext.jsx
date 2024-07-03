7fbb9ceaf709a0ede52c0c02d02acae91e5cd9ea
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-02 18:51:33 -0600

    remove console log

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -4,14 +4,12 @@ export const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
 
-    const [videos, setVideos] = useState([]);
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         const getData = async () => {
             const resolve = await fetch("http://localhost:5000/categories");
             const data = await resolve.json();
-            console.log(data);
             setCategories([...data]);
         };
 
