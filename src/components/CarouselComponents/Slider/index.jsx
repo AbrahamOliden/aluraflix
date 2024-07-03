8ca7ac51d60d571e23172671c83f65f9850bb0a9
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-02 19:48:56 -0600

    fix Slider's responsive breakpoints

--- src/components/CarouselComponents/Slider/index.jsx
+++ src/components/CarouselComponents/Slider/index.jsx
@@ -15,21 +15,14 @@ function MySlider({ videos, color }) {
         slidesToScroll: 3,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 768,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 320,
+                breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
