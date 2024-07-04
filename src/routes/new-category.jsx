58abdd5003f37e9eddfb2cc70f2fdb2cd0172965
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-03 21:05:29 -0600

    remove previous changes

--- src/routes/new-category.jsx
+++ src/routes/new-category.jsx
@@ -1,6 +1,5 @@
 import DefaultPage from "../components/DefaultPage";
 import NewCategory from "../components/Registry/NewCategory";
-import Form from "../components/Form"
 
 import React from 'react'
 
