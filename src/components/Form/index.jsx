a5a5e8d4f0bc30d6f6ba2b964faced7838e9580f
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-03 19:07:43 -0600

    import Category and Video components and create StyledField

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import CategoryForm from "./CategoryForm";
+import VideoForm from "./VideoForm";
+
+const StyledField = styled.fieldset`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 60px 30px;
+    border: none;
+`;
