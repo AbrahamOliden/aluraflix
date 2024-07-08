a5a140a7c4bdbf44cff36be9f2a9d4851573d796
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-07 19:37:25 -0600

    remove all react-router logic and reimport App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root.jsx';
-import NotFound from './routes/not-found.jsx';
-import NewVideoRoute from './routes/new-video.jsx';
-import NewCategoryRoute from './routes/new-category.jsx';
 import './index.css'
+import App from './App.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />
-  },
-  {
-    path: "new-video",
-    element: <NewVideoRoute />
-  },
-  {
-    path: "new-category",
-    element: <NewCategoryRoute />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <App />
   </React.StrictMode>
 )
