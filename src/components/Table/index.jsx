f8ad6320640282abda4e304e26be30c2b6be1737
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-08 16:23:18 -0600

    create first draft of Table component

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,14 @@
+function Table() {
+    return (
+        <table>
+            <colgroup>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </colgroup>
+        </table>
+    );
+};
+
+export default Table;
