f869ec3aff5aa284a0a8fa853299724465fab40d
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-07 19:54:26 -0600

    import react router and add necesory logic for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import GlobalContextProvider from "./context/GlobalContext";
-import Banner from "./components/Banner";
 import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NewVideo from "./components/Registry/NewVideo";
+import NewCategory from "./components/Registry/NewCategory";
+import GlobalStyles from "./GlobalStyles";
+// import NotFound from "./routes/not-found";
 
 function App() {
 
   return (
+    <BrowserRouter>
+      <GlobalStyles>
+        <Header />
         <GlobalContextProvider>
-          <Banner id="banner" />
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="new-video" element={<NewVideo />} />
+            <Route path="new-category" element={<NewCategory />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
         </GlobalContextProvider>
+        <Footer />
+      </GlobalStyles>
+    </BrowserRouter>
   );
 };
 
