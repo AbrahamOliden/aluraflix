785a0598678ff4ba79166d46b916f0d57720fd5c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-08 18:16:46 -0600

    map over GlobalContext categories to display data in a table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,12 +1,35 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalContext";
+
 function Table() {
+    const { categories } = useContext(GlobalContext);
+
     return (
-        <table>
-            <colgroup>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </colgroup>
+        <table align="center" border="1" style={{ width: "100%" }} >
+            <thead>
+                <tr>
+                    <th scope="col" >Name</th>
+                    <th scope="col" >Description</th>
+                    <th scope="col" >Edit</th>
+                    <th scope="col" >Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    categories.map(category => (
+                        <tr key={category.title} >
+                            <th scope="row" >{category.title}</th>
+                            <td>{category.description}</td>
+                            <td>
+                                <button>Edit</button>
+                            </td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
         </table>
     );
 };
