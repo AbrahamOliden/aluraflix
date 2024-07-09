f8cd6e4583f317372c7e8b43dd52a31e20e9caf4
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-08 20:42:10 -0600

    create more styled components

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -16,33 +16,65 @@ const StyledTableHead = styled.thead`
     font-weight: 500;
     tr {
         th {
-            padding: 10px 0;
-        }
+            padding: 12px 20px;
+        };
+        #name {
+            text-align: left;
+        };
         #description {
             width: 50%;
         };
         #edit, #delete {
             width: 10%;
-        }
-    }
+        };
+    };
+`;
+
+const CategoryTitle = styled.th`
+    font-size: 1.2rem;
+    font-weight: 600;
+    text-align: left;
+    padding: 12px 20px;
+`
+
+const CategoryDescription = styled.td`
+    font-size: 1.1rem;
+    font-weight: 400;
+    text-align: left;
+    padding: 12px 20px;
 `;
 
 const TableButton = styled.button`
     width: 100%;
-    margin: 0;
-    padding: 12px 20px;
+    margin: auto;
+    padding: 12px 20px ;
     border: none;
     background-color: transparent;
     color: var(--white);
     text-align: center;
     font-size: 1rem;
     cursor: pointer;
+    box-sizing: border-box;
+    transition: transform 0.5 ease;
+    &:hover {
+        background-color: rgba(34, 113, 209, 0.7);
+        color: var(--black);
+        font-weight: 600;
+    };
+    &:focus {
+        transform: scale(1.05);
+        border: 5px solid var(--blue);
+        box-shadow: 0 0 5px 5px rgba(34, 113, 209, 0.7);
+        color: var(--blue);
+        font-weight: 600;
+    }
+    &:active {
+        background-color: var(--blue);
+        color: var(--white);
+        font-weight: 600;
+    }
 `;
 
-const CategoryDescription = styled.td`
-    font-size: 1rem;
-    font-weight: 400;
-`;
 
 function Table() {
     const { categories } = useContext(GlobalContext);
@@ -61,7 +93,7 @@ function Table() {
                 {
                     categories.map(category => (
                         <tr key={category.title} >
-                            <th scope="row" >{category.title}</th>
+                            <CategoryTitle scope="row" >{category.title}</CategoryTitle>
                             <CategoryDescription>{category.description}</CategoryDescription>
                             <td>
                                 <TableButton>Edit</TableButton>
