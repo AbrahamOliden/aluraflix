5279ea4360baa2a55fe7be7d1e8b9057b5e38e2e
AbrahamOliden <abrahamoliden@protonmail.com>
2024-07-10 16:38:39 -0600

    create initialState object and reducer function

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -2,6 +2,23 @@ import { createContext, useState, useEffect } from "react";
 
 export const GlobalContext = createContext();
 
+const initialState = {
+    categories: [],
+    newVideo: {},
+    newCategory: {}
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_CATEGORIES":
+            return {...state, categories: action.payload};
+        case "SET_NEW_VIDEO":
+            return {...state, newVideo: action.payload};
+        case "SET_NEW_CATEGORY":
+            return {...state, newCategory: action.payload};
+    };
+}
+
 const GlobalContextProvider = ({ children }) => {
 
     const [categories, setCategories] = useState([]);
